09229c5ffaa65cabc28685e98e1c2f44942e3e06
BJ Miller <133471988+glimmeffros@users.noreply.github.com>
2023-05-17 16:38:47 -0400

    Eliminated Home menu item.

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 let menuBar = document.querySelector("#nav-bar");
+let menuTitle = document.querySelector("#nav-title");
 
 menuBar.addEventListener("click", function(e) {
   e.preventDefault();
 
-  let underlined = document.querySelector("ul .active");
+  let underlined = document.querySelector(".active");
+  if (underlined) {
+    underlined.classList.remove("active");
+  }
+  let selected = e.target;
+  selected.classList.add("active");
+  
+  let targetPage = selected.getAttribute("href");
+  if (targetPage) {
+    let visible = document.querySelector(".page.active");
+    if (visible) {
+      visible.classList.remove("active");
+    }
+    let target = document.querySelector(targetPage);
+    target.classList.add("active");
+  }
+});
+
+menuTitle.addEventListener("click", function(e) {
+  e.preventDefault();
+
+  let underlined = document.querySelector(".active");
   if (underlined) {
     underlined.classList.remove("active");
   }
