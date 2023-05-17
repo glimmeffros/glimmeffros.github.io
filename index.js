dd05fcda0c7c86faaadb09660fcf9f6178f1982a
BJ Miller <133471988+glimmeffros@users.noreply.github.com>
2023-05-17 16:41:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,26 @@
+function menuListener(e) {
+  e.preventDefault();
+
+  let underlined = document.querySelector(".active");
+  if (underlined) {
+    underlined.classList.remove("active");
+  }
+  let selected = e.target;
+  selected.classList.add("active");
+  
+  let targetPage = selected.getAttribute("href");
+  if (targetPage) {
+    let visible = document.querySelector(".page.active");
+    if (visible) {
+      visible.classList.remove("active");
+    }
+    let target = document.querySelector(targetPage);
+    target.classList.add("active");
+  }
+}
+
 let menuBar = document.querySelector("#nav-bar");
 let menuTitle = document.querySelector("#nav-title");
 
-menuBar.addEventListener("click", function(e) {
-  e.preventDefault();
-
-  let underlined = document.querySelector(".active");
-  if (underlined) {
-    underlined.classList.remove("active");
-  }
-  let selected = e.target;
-  selected.classList.add("active");
-  
-  let targetPage = selected.getAttribute("href");
-  if (targetPage) {
-    let visible = document.querySelector(".page.active");
-    if (visible) {
-      visible.classList.remove("active");
-    }
-    let target = document.querySelector(targetPage);
-    target.classList.add("active");
-  }
-});
-
-menuTitle.addEventListener("click", function(e) {
-  e.preventDefault();
-
-  let underlined = document.querySelector(".active");
-  if (underlined) {
-    underlined.classList.remove("active");
-  }
-  let selected = e.target;
-  selected.classList.add("active");
-  
-  let targetPage = selected.getAttribute("href");
-  if (targetPage) {
-    let visible = document.querySelector(".page.active");
-    if (visible) {
-      visible.classList.remove("active");
-    }
-    let target = document.querySelector(targetPage);
-    target.classList.add("active");
-  }
-});
+menuBar.addEventListener("click", menuListener);
+menuTitle.addEventListener("click", menuListener);
