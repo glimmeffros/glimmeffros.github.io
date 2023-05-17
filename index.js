0acaffb26bfd2ab65fabc9f6387ffa20b02ea0c7
BJ Miller <133471988+glimmeffros@users.noreply.github.com>
2023-05-17 10:49:33 -0400

    Fixed navbar

--- index.js
+++ index.js
@@ -2,14 +2,21 @@ let menuBar = document.querySelector("#nav-bar");
 
 menuBar.addEventListener("click", function(e) {
   e.preventDefault();
-  let nav = e.target;
-  let targetPage = nav.getAttribute("href");
+
+  let underlined = document.querySelector(".active");
+  if (underlined) {
+    underlined.classList.remove("active");
+  }
+  let selected = e.target;
+  selected.classList.add("active");
+  
+  let targetPage = selected.getAttribute("href");
   if (targetPage) {
     let visible = document.querySelector(".page.active");
     if (visible) {
       visible.classList.remove("active");
     }
     let target = document.querySelector(targetPage);
-    target.classList.toggle("active");
+    target.classList.add("active");
   }
 });
